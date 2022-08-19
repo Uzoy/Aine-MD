7c46b4eebe22548b918b7e7d51e9ffd241077c4e
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-08-19 17:26:06 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ • Telkomsel [082284519515]
+│ • Axis [083189131519]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
